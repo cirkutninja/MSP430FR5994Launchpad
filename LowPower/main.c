086953f291670530ef3926e8ebaf7341da4ca170
Kevin Christensen <kchristensen@actall.com>
2016-11-01 17:26:14 -0600

    -Partially working code example base don the Design Workshop lab files that implements a basic approach to a bare bones test rig
    Signed-off-by: Kevin Christensen <kchristensen@actall.com>

--- LowPower/main.c
+++ LowPower/main.c
@@ -3,82 +3,111 @@
 
 
 void initGPIO(void);
+void initClocks(void);
+
+
+#define greenLED 		GPIO_PIN0
+#define redLED			GPIO_PIN1
+#define LEDPort			GPIO_PORT_P1
+#define pushButton1		GPIO_PIN5
+#define	pushButton2		GPIO_PIN6
+
+#define	LF_CRYSTAL_FREQUENCY_IN_HZ	32768
+#define HF_CRYSTAL_FREQUENCY_IN_HZ	0
+
+
+uint32_t myACLK = 0;
+uint32_t mySMCLK = 0;
+uint32_t myMCLK = 0;
 /*
+ *
  * main.c
  */
 int main(void)
 {
+	WDT_A_initIntervalTimer(WDT_A_BASE,
+			WDT_A_CLOCKSOURCE_ACLK,
+			WDT_A_CLOCKDIVIDER_512K);
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer to allow
+    							//allow everything to intialize
 
+	SFR_clearInterrupt(SFR_WATCHDOG_INTERVAL_TIMER_INTERRUPT);
+	SFR_enableInterrupt(SFR_WATCHDOG_INTERVAL_TIMER_INTERRUPT);
 
-	CS_initClockSignal(CS_ACLK,
-			CS_VLOCLK_SELECT,
-			CS_CLOCK_DIVIDER_1);
-	CS_initClockSignal(CS_MCLK,
-				CS_VLOCLK_SELECT,
-				CS_CLOCK_DIVIDER_1);
-	CS_initClockSignal(CS_SMCLK,
-				CS_VLOCLK_SELECT,
-				CS_CLOCK_DIVIDER_1);
-
-	CS_turnOffSMCLK();
-
-
-
-
-	//WDT_A_initIntervalTimer(WDT_A_BASE,
-			//WDT_A_CLOCKSOURCE_ACLK,
-			//WDT_A_CLOCKDIVIDER_512K);
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-
-
-
-	//SFR_clearInterrupt(SFR_WATCHDOG_INTERVAL_TIMER_INTERRUPT);
-	//SFR_enableInterrupt(SFR_WATCHDOG_INTERVAL_TIMER_INTERRUPT);
-	
+	//Initialization routines
     initGPIO();
+    initClocks();
 
+    WDT_A_start(WDT_A_BASE);
+    _enable_interrupts();
+    //PMM_turnOffRegulator();
+    //PMM_disableSVSH();
 
+    while(1)
+    {
+    	_low_power_mode_3(); //Enter Low Power Mode
+    	_nop();
+    }
 
-    // Set P1.0 to output direction
-    //GPIO_setAsOutputPin(GPIO_PORT_P1,GPIO_PIN0);
-    //GPIO_setOutputLowOnPin(GPIO_PORT_P1,GPIO_PIN0);
-    // Disable the GPIO power-on default high-impedance mode
-    // to activate previously configured port settings
-    PMM_unlockLPM5();
-
-    //WDT_A_start(WDT_A_BASE);
-    //_enable_interrupts();
-    PMM_turnOffRegulator();
-    PMM_disableSVSH();
-    _low_power_mode_4(); //Enter LPM3
-
-           //_nop();
-
-
-    return 0;
 }
 
-#pragma vector=WDT_VECTOR
-__interrupt
-
-void WDT_A_ISR(void)
+void initClocks(void)
 {
-	volatile uint32_t i;
-	volatile uint32_t y;
-	for(y=4;y>0;y--)
-	{
-		GPIO_toggleOutputOnPin(GPIO_PORT_P1,GPIO_PIN0);
-		for(i=100;i>0;i--)
-		{
-			_nop();
-		}
-	}
+	//set the crystal frequencies attached to the LFXT and HFXT oscillator pins
+	//so that driverlib knows how fast they are (needed for the clock 'get' functions)
+	CS_setExternalClockSource(
+			LF_CRYSTAL_FREQUENCY_IN_HZ,
+			HF_CRYSTAL_FREQUENCY_IN_HZ
+			);
 
-	GPIO_setOutputLowOnPin(GPIO_PORT_P1,GPIO_PIN0);
+//#ifdef __DEBUG
+	//verify if the default clock settings are as expected
+	myACLK = CS_getACLK();
+	mySMCLK = CS_getSMCLK();
+	myMCLK = CS_getMCLK();
+//#endif
+
+	//set FRAM controller waitstates to 1 when MCLK>8MHz (per datasheet)
+	//Refer to the "Non-volatile memory" chapter for more details
+	//FRAMCtl_configureWaitStateControl(FRAMCTL_ACCESS_TIME_CYCLES_1);
+
+	//Set DCO to run at 8 MHz
+	CS_setDCOFreq(CS_DCORSEL_1, CS_DCOFSEL_3);
+
+	//Configure Clocks
+	//Set ACLK to us VLO as its oscillator source (~10kHz)
+	CS_initClockSignal(
+			CS_ACLK,				//Clock we're configuring
+			CS_VLOCLK_SELECT,		//Clock source
+			CS_CLOCK_DIVIDER_1		//clock divider
+			);
+	//Set SMCLK to use DCO as its source
+	CS_initClockSignal(
+			CS_SMCLK,				//Clock we're configuring
+			CS_DCOCLK_SELECT,		//Clock source
+			CS_CLOCK_DIVIDER_1		//clock divider
+			);
+
+	//Set the MCLK to use the VLO clock
+	/*
+	CS_initClockSignal(
+				CS_ACLK,				//Clock we're configuring
+				CS_VLOCLK_SELECT,		//Clock source
+				CS_CLOCK_DIVIDER_1		//clock divider
+				);
+				*/
+//#ifdef __DEBUG
+	//verify the modified clock settings are as expected
+	myACLK = CS_getACLK();
+	mySMCLK = CS_getSMCLK();
+	myMCLK = CS_getMCLK();
+//#endif
+
 }
 
 void initGPIO(void)
 {
+
 	P1DIR = 0xFF;
 	P1OUT = 0x00;
 
@@ -114,4 +143,96 @@ void initGPIO(void)
 
 	PDDIR = 0xFF;
 	PDOUT = 0x00;
+
+	//set LED pin direction to output and turn off
+	GPIO_setAsOutputPin(LEDPort,
+				greenLED +
+				redLED);
+
+	GPIO_setOutputLowOnPin(LEDPort,
+			greenLED +
+			redLED);
+
+	PMM_unlockLPM5();
+
+	//Configure our Launchpad Push Buttons
+	//inputs with pull-up resistors
+	GPIO_setAsInputPinWithPullUpResistor(
+			GPIO_PORT_P5, pushButton1 +
+			pushButton2);
+
+	//Configure GPIO pins ot trigger interrupt on a Low-to-High trnasition
+	GPIO_selectInterruptEdge(
+			GPIO_PORT_P5,
+			pushButton1 +
+			pushButton2,
+			GPIO_LOW_TO_HIGH_TRANSITION);
+
+	//Clear the interrupt flags for the push button pins
+	GPIO_clearInterrupt(
+			GPIO_PORT_P5,
+			pushButton1 +
+			pushButton2);
+
+	//Enable the interrupt bits for the push button pins
+	GPIO_enableInterrupt(
+			GPIO_PORT_P5,
+			pushButton1 +
+			pushButton2);
+
+
+
+	//Configure external crystal pins
+	//Set LFXT to crystal input (rather than GPIO)
+	//HFXT is not used, so we ignore those pins
+	GPIO_setAsPeripheralModuleFunctionInputPin(
+			GPIO_PORT_PJ,
+			GPIO_PIN4 +		//LFXIN PJ.4
+			GPIO_PIN5,		//LFXOUT PJ.5
+			GPIO_PRIMARY_MODULE_FUNCTION);
+
+
+
+//#ifdef __DEBUG
+	//Output MSP clock signal to external pins for debugging purposes
+	GPIO_setAsPeripheralModuleFunctionInputPin(
+			GPIO_PORT_P5,
+			GPIO_PIN7,		//MCLK on P5.7
+			GPIO_TERNARY_MODULE_FUNCTION);
+
+	GPIO_setAsPeripheralModuleFunctionInputPin(
+			GPIO_PORT_P3,
+			GPIO_PIN4,		//SMCLK on P3.4
+			GPIO_SECONDARY_MODULE_FUNCTION);
+//#endif
+
+}
+
+//Interrupt Service Routine
+#pragma vector=PORT5_VECTOR
+__interrupt void pushbutton_ISR(void)
+{
+
+	if(P5IV_P5IFG5)
+	{
+			GPIO_toggleOutputOnPin(LEDPort,redLED);
+	}
+
+}
+
+#pragma vector=WDT_VECTOR
+__interrupt void WDT_A_ISR(void)
+{
+	volatile uint32_t i;
+	volatile uint32_t y;
+	for(y=4;y>0;y--)
+	{
+		GPIO_toggleOutputOnPin(LEDPort,greenLED + redLED);
+		for(i=100;i>0;i--)
+		{
+			_nop();
+		}
+	}
+
+	GPIO_setOutputLowOnPin(GPIO_PORT_P1,GPIO_PIN0);
 }
